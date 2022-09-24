f379323255dd41b1c9767a02fb410b728eb45e05
IriS <mhuynk1005@gmail.com>
2022-09-24 15:44:52 +0700

    Problem 4

---
+++ Problem 4/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4
+{
+    internal class Person : IComparable<Person>
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Person() : this("No name", 1)
+        {
+
+        }
+        public Person(int age) : this("No name", age)
+        {
+
+        }
+
+        public int CompareTo(Person other)
+        {
+            if (other == null) return 1;
+            else return this.name.CompareTo(other.name);
+        }
+    }
+}
---
+++ Problem 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using Problem_4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Person> myList = new List<Person>();
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            string info = Console.ReadLine();
+            string[] infoArray = info.Split();
+            if (int.Parse(infoArray[1]) > 30)
+            {
+                Person myPerson = new Person(infoArray[0], int.Parse(infoArray[1]));
+                myList.Add(myPerson);
+            }
+        }
+        Console.WriteLine();
+        myList = myList.OrderBy(o => o.name).ToList();
+        foreach (Person myPerson in myList)
+        {          
+            Console.WriteLine("{0} - {1}", myPerson.name, myPerson.age); 
+        }
+    }
+}

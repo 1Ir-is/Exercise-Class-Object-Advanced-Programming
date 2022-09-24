099202bf0fc2015e24cdeaf2c10b2413cdd65b0c
IriS <mhuynk1005@gmail.com>
2022-09-24 15:47:47 +0700

    Problem 5

---
+++ Problem 5/DateModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_5
+{
+    internal class DateModifier
+    {
+        public static int DifferenceDate(string date1String, string date2String)
+        {
+            DateTime date1 = DateTime.ParseExact(date1String, "yyyy M d", CultureInfo.InvariantCulture);
+            Console.WriteLine(date1);
+            DateTime date2 = DateTime.ParseExact(date2String, "yyyy M d", CultureInfo.InvariantCulture);
+            Console.WriteLine(date2);
+            var difference = date1 - date2;
+
+            Console.WriteLine(difference);
+            return Math.Abs(difference.Days);
+        }
+    }
+}
---
+++ Problem 5/Program.cs
@@ -0,0 +1,13 @@
+﻿using Problem_5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string date1 = Console.ReadLine();
+        string date2 = Console.ReadLine();
+        int result = DateModifier.DifferenceDate(date1, date2);
+        Console.WriteLine();
+        Console.WriteLine(result);
+    }
+}

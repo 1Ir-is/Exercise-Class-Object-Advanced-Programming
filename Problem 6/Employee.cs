f52006731f911d0a053b4af3ab3bf00d236c560b
IriS <mhuynk1005@gmail.com>
2022-09-24 15:51:49 +0700

    Problem 6

---
+++ Problem 6/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6
+{
+    internal class Employee
+    {
+        public string name;
+        public double salary;
+        public string position;
+        public string department;
+        public string email;
+        public int age;
+        public Employee(string name, double salary, string position, string department, string email, int age)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.department = department;
+            this.email = email;
+            this.age = age;
+        }
+        public Employee(string name, double salary, string position, string department)
+            : this(name, salary, position, department, "n/a", -1)
+        {
+
+        }
+        public Employee(string name, double salary, string position, string department, string email)
+            : this(name, salary, position, department, email, -1)
+        {
+
+        }
+        public Employee(string name, double salary, string position, string department, int age)
+            : this(name, salary, position, department, "n/a", age)
+        {
+
+        }
+        public override string ToString()
+        {
+            return name + " " + salary + " " + email + " " + age;
+        }
+    }
+}
---
+++ Problem 6/Program.cs
@@ -0,0 +1,38 @@
+﻿using Problem_6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Employee> myList = new List<Employee>();
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            string info = Console.ReadLine();
+            string[] infoArray = info.Split();
+            Employee employee;
+            if (infoArray.Length == 4)
+                employee = new Employee(infoArray[0], double.Parse(infoArray[1]), infoArray[2], infoArray[3]);
+            else if (infoArray.Length == 6)
+                employee = new Employee(infoArray[0], double.Parse(infoArray[1]), infoArray[2], infoArray[3], infoArray[4], int.Parse(infoArray[5]));
+            else if (infoArray[4].Contains("@"))
+                employee = new Employee(infoArray[0], double.Parse(infoArray[1]), infoArray[2], infoArray[3], infoArray[4]);
+            else employee = new Employee(infoArray[0], double.Parse(infoArray[1]), infoArray[2], infoArray[3], int.Parse(infoArray[4]));
+            myList.Add(employee);
+        }
+        var best = myList.GroupBy(g => g.department)
+            .Select(g => new
+            {
+                Average = g.Average(e => e.salary),
+                Department = g.Key
+            }).OrderByDescending(d => d.Average).First();
+        Console.WriteLine();
+        Console.WriteLine("Highest Average Salary:" + best.Department);
+        myList = myList.FindAll(g => g.department == best.Department).OrderByDescending(g => g.salary).ToList();
+        foreach (Employee employee in myList)
+        {
+            if (employee.department == best.Department)
+                Console.WriteLine(employee);
+        }
+    }
+}

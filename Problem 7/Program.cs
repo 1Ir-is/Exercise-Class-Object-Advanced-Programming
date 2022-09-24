f398ee9e5f9c15d3b4d6b84db093b4b6e0d06a43
IriS <mhuynk1005@gmail.com>
2022-09-24 15:55:29 +0700

    Problem 7

---
+++ Problem 7/Program.cs
@@ -0,0 +1,44 @@
+﻿using Problem_7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var cars = new List<Car>();
+
+        var carCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < carCount; i++)
+        {
+            var carInfor = Console.ReadLine().Split();
+            var model = carInfor[0];
+            var fuel = decimal.Parse(carInfor[1]);
+            var fuelCost = decimal.Parse(carInfor[2]);
+
+            cars.Add(new Car(model, fuel, fuelCost));
+        }
+
+        var comandDrive = Console.ReadLine();
+        while (!comandDrive.Equals("End"))
+        {
+            var comandArgs = comandDrive.Split();
+            var model = comandArgs[1];
+            var distance = decimal.Parse(comandArgs[2]);
+
+            var currentCar = cars.FirstOrDefault(c => c.Model == model);
+
+            if (!Car.CanDrive(currentCar, distance))
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+
+            comandDrive = Console.ReadLine();
+        }
+        Console.WriteLine();
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.Fuel:f2} {(int)car.TraveledDistance}");
+        }
+    }
+}

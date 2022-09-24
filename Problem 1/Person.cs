db4fea310a18e9f4588ac45727588e7b1cc252cf
IriS <mhuynk1005@gmail.com>
2022-09-24 15:20:01 +0700

    Problem 1

---
+++ Problem 1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+    }
+}
---
+++ Problem 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Problem_1;
+using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Type personType = typeof(Person);
+        FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+        Console.WriteLine(fields.Length);
+
+    }
+}

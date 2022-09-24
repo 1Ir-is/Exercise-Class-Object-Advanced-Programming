3cdd3cfe905f0c10c6c89cd33a3557925ebf331b
IriS <mhuynk1005@gmail.com>
2022-09-24 15:40:17 +0700

    Problem 3

---
+++ Problem 3/Family.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    internal class Family
+    {
+        List<Person> families;
+        public Family()
+        {
+            families = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            families.Add(member);
+        }
+        
+        public Person GetOldestMember()
+        {
+            int max = int.MinValue;
+            int maxIndex = -1;
+            for (int i = 0; i < families.Count; i++)
+            {
+                if (families[i].age > max)
+                {
+                    max = families[i].age;
+                    maxIndex = i;
+                }
+            }
+            if (maxIndex != -1) return families[maxIndex];
+            else return null;
+        }
+    }
+}
---
+++ Problem 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Problem_3;
+using System.Reflection;
+
+MethodInfo oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
+MethodInfo addMemberMethod = typeof(Family).GetMethod("AddMember");
+if (oldestMemberMethod == null || addMemberMethod == null)
+{
+    throw new Exception();
+}
+Family myFamily = new Family();
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+{
+    string info = Console.ReadLine();
+    string[] infoArray = info.Split();
+    Person myPerson = new Person(infoArray[0], int.Parse(infoArray[1]));
+    myFamily.AddMember(myPerson);
+}
+Person oldestPerson = myFamily.GetOldestMember();
+Console.WriteLine();
+Console.WriteLine("{0} {1}", oldestPerson.name, oldestPerson.age);
